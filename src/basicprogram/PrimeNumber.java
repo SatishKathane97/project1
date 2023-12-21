2e75af2800fafc84603613afddc65dd46167361a
Satish Kathane <Satish Kathane@Satish>
2023-12-22 00:48:15 +0530

    Prime Number

---
+++ src/basicprogram/PrimeNumber.java
@@ -0,0 +1,19 @@
+package basicprogram;
+
+public class PrimeNumber {
+   public static void main(String[] args) {
+	int n=27;
+	int count=0;
+	for(int i=2;i<n/2;i++) {
+		if(n%i==0) {
+			count++;
+		}
+	}
+	if(count==0) {
+		System.out.println("It is Prime Number");
+	}
+	else {
+		System.out.println("It is not Prime Number");
+	}
+}
+}
